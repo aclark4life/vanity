e7caceefe9ec71561c224c78c917ff9979fb282d
Alex Clark <aclark@aclark.net>
2011-11-28 23:33:27 -0500

    License nit

--- vanity.py
+++ vanity.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (C) 2011 Alex Clark                                               #
+# Copyright (C) 2011 ACLARK.NET, LLC                                          #
 #                                                                             #
 # This program is free software; you can redistribute it and/or               #
 # modify it under the terms of the GNU General Public License                 #
