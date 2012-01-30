8d29630d96080913f306dc47b3a6a94343ba2e69
Alex Clark <aclark@aclark.net>
2012-01-30 01:32:40 -0500

    Alpha order and ini to dict

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-
 from setuptools import setup
 import os
 
 setup(
-    name='vanity',
-    version='1.2.0',
+    author='Alex Clark',
+    author_email='Alex Clark',
     description='Easy access to PyPI download stats',
-    long_description=(
-        open('README.rst').read() +
+    entry_points={
+        'console_scripts': 'vanity = vanity:main'
+    },
+    include_package_data=True,
+    long_description=(open('README.rst').read() +
         open(os.path.join('docs', 'HISTORY.txt')).read()),
-    py_modules=['vanity'],
-    author = 'Alex Clark',
-    author_email = 'Alex Clark',
-    maintainer = 'pythonpackages',
-    maintainer_email = 'info@pythonpackages.com',
+    maintainer='pythonpackages',
+    maintainer_email='info@pythonpackages.com',
+    name='vanity',
+    py_modules=[
+        'vanity'
+    ],
     url='https://github.com/aclark4life/vanity',
-    entry_points = """
-    [console_scripts]
-    vanity = vanity:main
-    """
-    include_package_data = True,
+    version='1.2.0',
 )
