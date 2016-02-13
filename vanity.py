39f27baea81aa1bd59b06c218d5b725600f89adf
Alex Clark <aclark@aclark.net>
2016-02-13 13:12:09 -0500

    Update copyright date (?)
    No idea what this means, other than "I worked on it in these years" (http://stackoverflow.com/questions/2390230/do-copyright-dates-need-to-be-updated)

--- vanity.py
+++ vanity.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011-2015 Alex Clark
+# Copyright (C) 2011-2016 Alex Clark
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
