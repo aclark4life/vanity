f16e2d65f0cf1756cac2c1bdaa9e0c4a54fa03af
mattjegan <matthewj.egan@hotmail.com>
2016-08-24 13:54:55 +1000

    Updated travis and setup

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(author='Alex Clark',
       author_email='aclark@aclark.net',
       classifiers=[
           'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.2',
