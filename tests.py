acd261543b7d457898fc6d8c6350b2aa50a5c818
Andy Moran <amoran@FGS.LOCAL>
2016-10-10 16:07:08 +0100

    Add tests for normalize function
    Cases: Fake package, django, None, empty string
    
    Noticed that normalize called with "" runs rather long and
    returns "" still, perhaps this could be refactored.

--- tests.py
+++ tests.py
@@ -1,6 +1,41 @@
 import unittest
 
+import vanity
 
-class TestSuite(unittest.TestSuite):
+class testNormalize(unittest.TestCase):
     """
+    A test class for the normalize method.
     """
+
+    def testFakePackage(self):
+        self.assertRaises(ValueError, 
+                          vanity.normalize, 
+                          "FAKEPACKAGE1@!")
+
+    def testDjango(self):
+        n = vanity.normalize("dJaNgO")
+        self.assertEqual(n, "Django")
+
+    def testNone(self):
+        n = vanity.normalize(None)
+        self.assertEqual(n, "none")
+
+    def testEmpty(self):
+        """
+        Note: this test is rather slow to run, 
+        perhaps normalize could be refactored to check this
+        and kick out faster.
+        """
+        n = vanity.normalize("")
+        self.assertEqual(n, "")
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(testNormalize))
+    return suite
+
+if __name__ == '__main__':
+    suite = suite()
+    unittest.TextTestRunner().run(suite())
+
+    
